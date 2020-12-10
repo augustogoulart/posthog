57c1947c86a14fa847e9390db31e21b31f501697
Karl-Aksel Puulmann <oxymaccy@gmail.com>
2020-12-10 21:33:37 +0200

    make it possible to filter by action

--- posthog/models/filters/sessions_filter.py
+++ posthog/models/filters/sessions_filter.py
@@ -1,28 +1,33 @@
+import json
 from typing import Any, Dict, Optional
 
 from django.http import HttpRequest
 
 from posthog.constants import DISTINCT_ID_FILTER
-from posthog.models import Filter
+from posthog.models import Entity, Filter
+
+json_load_if = lambda data: json.loads(data) if data is not None else None
 
 
 class SessionsFilter(Filter):
     distinct_id: Optional[str]
     duration_operator: Optional[str]  # lt, gt
     _duration: Optional[str]
+    action_filter: Optional[Entity]
 
     def __init__(self, data: Dict[str, Any] = {}, request: Optional[HttpRequest] = None, **kwargs) -> None:
         super().__init__(data, request, **kwargs)
         if request:
-            data = {
-                **request.GET.dict(),
-            }
+            data = {**request.GET.dict(), "action_filter": json_load_if(request.GET.get("action_filter"))}
         elif not data:
             raise ValueError("You need to define either a data dict or a request")
 
         self.distinct_id = data.get(DISTINCT_ID_FILTER)
         self.duration_operator = data.get("duration_operator")
         self._duration = data.get("duration")
+        self.action_filter = None
+        if data.get("action_filter") is not None:
+            self.action_filter = Entity(data["action_filter"])
 
     @property
     def duration(self) -> float:
